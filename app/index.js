ebdb74ec04b4202814f10c1eb525d9dee8aef749
Mauricio Rodriguez <hyde1942@gmail.com>
2018-07-18 23:09:32 -0600

    [*] finished some excercies

--- app/index.js
+++ app/index.js
@@ -1,1 +1,49 @@
-console.log('Hello Webpack 4!.');
+let a = 'Hello from mars!';
+console.log(a);
+
+{
+	let a = 'Hello from Venus!...';
+	console.log(a);
+}
+
+function run () {
+	let b = 2;
+	console.log(b);
+	{
+		let b = 3;
+		console.log(b);
+	}
+}
+
+run();
+
+const salary  = 2400;
+
+console.log(salary);
+
+//salary = 30000; throws an error!....
+
+const array = [1,2,3];
+
+//array = [1,2,3,4]; throws an error;
+
+//array.push(4); works it pushes the item into the array!.
+
+//array = 3 + 1; throws an error!.
+
+
+console.log(array);
+
+
+
+function print () {
+	let saber = {
+		luke : 'blue',
+		vader: 'red'
+	}
+
+	let message = `Luke first uses a ${saber.luke}-colored lightsaber, But his father, Vader uses a ${saber.vader}-colored lightsaber!. `
+	console.log(message); 
+}
+
+print();
